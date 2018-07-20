53bdf176c6b5e6746fc6376d0d24dda33a32ee4b
Pierre-quentin Deguingand <pdeguing@e1z2r3p27.42.us.org>
2018-07-19 17:29:32 -0700

    Updated ft_strnequ()

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/11 09:19:47 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/07/11 09:22:51 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/07/19 17:28:51 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,7 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	if (ft_strlen(s1) != ft_strlen(s2))
-		return (0);
-	while (i < n && s1[i] && s2[i])
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	return (1);
+	if (ft_strncmp(s1, s2, n) == 0)
+		return (1);
+	return (0);
 }
