7d93e84be480157203720869a47497dc2754c740
Pierre-quentin Deguingand <pdeguing@e1z2r3p27.42.us.org>
2018-07-19 17:27:08 -0700

    Updated ft_strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/11 09:15:41 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/07/12 17:31:34 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/07/19 17:24:33 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,7 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-	size_t	i;
-
-	i = 0;
-	if (ft_strlen(s1) != ft_strlen(s2))
-		return (0);
-	while (s1[i] && s2[i])
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	return (1);
+	if (ft_strcmp(s1, s2) == 0)
+		return (1);
+	return (0);
 }
