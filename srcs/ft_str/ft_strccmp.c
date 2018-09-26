c3785f485b09f030da53d877351f1d513a331164
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2018-09-26 15:07:06 -0700

    Fixing bug if arg1 is c and arg2 is null

--- srcs/ft_str/ft_strccmp.c
+++ srcs/ft_str/ft_strccmp.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 13:36:38 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/09/26 13:40:50 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/09/26 14:44:27 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int		ft_strccmp(const char *s1, const char *s2, char c)
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] && s2[i])
 	{
-		if (s1[i] == c || s2[i] == c)
-			break ;
 		i++;
+		if ((s1[i] == c || s2[i] == c) && (!s1[i] || !s2[i]))
+			return (0);
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
