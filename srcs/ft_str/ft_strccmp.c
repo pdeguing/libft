db1c62e5b23c752e0b2bc930f67d8b343e32973b
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2018-09-28 14:20:42 -0700

    Fixing bugs

--- srcs/ft_str/ft_strccmp.c
+++ srcs/ft_str/ft_strccmp.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 13:36:38 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/09/26 14:44:27 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/09/28 14:12:04 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int		ft_strccmp(const char *s1, const char *s2, char c)
 	{
 		i++;
 		if ((s1[i] == c || s2[i] == c) && (!s1[i] || !s2[i]))
+			return (0);
+		if (s1[i] == c && s2[i] == c)
 			return (0);
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
